7fa5c6ec4d3580bf9412ff83bc58b3a6ac69eb4c
Peter A. Laudenslager <peter.laudenslager@gmail.com>
2015-01-27 11:29:52 -0800

    Added Apache license

--- DTree.py
+++ DTree.py
@@ -1,5 +1,23 @@
 __author__ = 'peter'
 
+'''
+Copyright 2014 Peter Laudenslager
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+   '''
+
+
 import uuid
 
 class Parent_ID:
@@ -14,7 +32,7 @@ class Node:
             raise ReferenceError('Illegal node type: {}'.format(node_type))
 
         if ID == None:
-            self.ID = str(uuid.uuid1()).split("-")[3]
+            self.ID = str(uuid.uuid1())
         else:
             self.ID = str(ID).strip().lower().replace(" ","")
 
--- DTree_tests.py
+++ DTree_tests.py
@@ -1,5 +1,22 @@
 __author__ = 'peter'
 
+'''
+Copyright 2014 Peter Laudenslager
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+   '''
+
 from DTree import Tree, Node, Parent_ID
 import unittest
 
@@ -70,7 +87,9 @@ class TestTreeFunctions(unittest.TestCase):
 
     def test_deep(self):
         # Verify that a very deep tree can be created
-        depth = 1000
+        depth = 974
+        # over 975 will generate a recursion depth error on the forward_solve
+
         self.tree.set_node(0,'D', 3)
         node_ID = 0
         for level in range(1, depth):
